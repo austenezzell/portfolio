1299b39b99464632b2ec42557446579c23256f58
Austen <austenezzell@gmail.com>
2015-06-25 16:11:06 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,255 @@
+/* ====================================
+ * Define paths
+ * ==================================== */
+var source = 'source';
+var build = 'build';
+
+/* ====================================
+ * Load required plug-ins
+ * ==================================== */
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var runSequence = require('run-sequence');
+var del = require('del');
+var es = require('event-stream');
+var bowerFiles = require('main-bower-files');
+
+var plumberConfig = {errorHandler: $.notify.onError("Error: <%= error.message %>")};
+
+/* ====================================
+ * Web server
+ * ==================================== */
+gulp.task('serve', ['watch'], function(){
+  browserSync({
+    server: {
+      baseDir: build
+    },
+    notify: false,
+    ghostMode: false
+  });
+});
+
+/* ====================================
+ * Styles
+ * ==================================== */
+gulp.task('styles', function () {
+  return gulp.src(source + '/scss/**/style.scss')
+    .pipe($.plumber(plumberConfig))
+    .pipe($.rubySass())
+    .pipe($.autoprefixer((["last 1 version", "> 1%", "ie 8", "ie 7"], { cascade: true })))
+    .pipe(gulp.dest(build + '/css/'));
+});
+
+
+/* ====================================
+ * Scripts
+ * ==================================== */
+ gulp.task('jshint', function() {
+  return gulp.src(source + '/js/script.js')
+    .pipe($.plumber(plumberConfig))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('default'));
+});
+
+gulp.task('scripts', function () {
+  return gulp.src([source + '/js/plugins.js', source + '/js/**/*'])
+    .pipe($.plumber(plumberConfig))
+    .pipe(gulp.dest(build + '/js'));
+});
+
+/* ====================================
+ * Images
+ * ==================================== */
+gulp.task('images', function() {
+  return gulp.src(source + '/img/**/*')
+    .pipe($.plumber(plumberConfig))
+    .pipe($.cache($.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest(build + '/img'));
+});
+
+/* ====================================
+ * HTML
+ * ==================================== */
+gulp.task('html-default', function() {
+  var vendorjs = gulp.src(bowerFiles())
+    .pipe($.plumber(plumberConfig))
+    .pipe($.filter('**/*.js'))
+    .pipe(gulp.dest(build + '/js'));
+
+  var vendorcss = gulp.src(bowerFiles())
+    .pipe($.plumber(plumberConfig))
+    .pipe($.filter('**/*.css'))
+    .pipe(gulp.dest(build + '/css'));
+
+  var scripts = gulp.src([source + '/js/plugins.js', source + '/js/**/*'])
+    .pipe($.plumber(plumberConfig))
+    .pipe(gulp.dest(build + '/js'));
+
+  var scriptsHead = gulp.src([source + '/js/vendor/modernizr.js'])
+    .pipe($.plumber(plumberConfig))
+    .pipe(gulp.dest(build + '/js'));
+
+  var styles = gulp.src(source + '/scss/**/style.scss')
+    .pipe($.plumber(plumberConfig))
+    .pipe($.rubySass())
+    .pipe($.autoprefixer((["last 1 version", "> 1%", "ie 8", "ie 7"], { cascade: true })))
+    .pipe(gulp.dest(build + '/css/'));
+
+  return gulp.src([
+      source + '/htdocs/**/*.html',
+      '!' + source + '/htdocs/_templates{,/**}'
+    ])
+    .pipe($.plumber(plumberConfig))
+    .pipe($.fileInclude({
+      prefix: '@@',
+      basepath: 'source/'
+    }))
+    .pipe($.inject(scriptsHead,
+      { ignorePath: [build, source],
+        addRootSlash: true,
+        starttag: '<!-- inject:head:{{ext}} -->'
+      }
+    ))
+    .pipe($.inject(es.merge(
+        vendorcss,
+        vendorjs
+      ),
+      {
+        name: 'bower',
+        ignorePath: [build, source],
+        addRootSlash: true
+      }
+      ))
+    .pipe($.inject(es.merge(
+      styles,
+      scripts
+    ),
+      {
+        ignorePath: [build, source],
+        addRootSlash: true
+      }
+    ))
+    .pipe(gulp.dest(build));
+});
+
+gulp.task('html-build', function() {
+  var vendorjs = gulp.src(bowerFiles())
+    .pipe($.plumber(plumberConfig))
+    .pipe($.filter('**/*.js'))
+    .pipe($.concat('vendor.js'))
+    .pipe($.uglify())
+    .pipe($.rename({suffix: '.min'}))
+    .pipe(gulp.dest(build + '/js'));
+
+  var vendorcss = gulp.src(bowerFiles())
+    .pipe($.plumber(plumberConfig))
+    .pipe($.filter('**/*.css'))
+    .pipe($.concat('vendor.css'))
+    .pipe($.minifyCss())
+    .pipe($.rename({suffix: '.min'}))
+    .pipe(gulp.dest(build + '/css'));
+
+  var scripts = gulp.src([source + '/js/plugins.js', source + '/js/**/*'])
+    .pipe($.plumber(plumberConfig))
+    .pipe($.concat('script.js'))
+    .pipe($.uglify())
+    .pipe($.rename({suffix: '.min'}))
+    .pipe(gulp.dest(build + '/js'));
+
+  var scriptsHead = gulp.src([source + '/js/vendor/modernizr.js'])
+    .pipe($.plumber(plumberConfig))
+    .pipe(gulp.dest(build + '/js'));
+
+  var styles = gulp.src(source + '/scss/**/*.scss')
+    .pipe($.plumber(plumberConfig))
+    .pipe($.rubySass())
+    .pipe($.concat('style.css'))
+    .pipe($.autoprefixer((["last 1 version", "> 1%", "ie 8", "ie 7"], { cascade: true })))
+    .pipe($.minifyCss())
+    .pipe($.rename({suffix: '.min'}))
+    .pipe(gulp.dest(build + '/css/'));
+
+  return gulp.src([
+      source + '/htdocs/**/*.html',
+      '!' + source + '/htdocs/_templates{,/**}'
+    ])
+    .pipe($.plumber(plumberConfig))
+    .pipe($.fileInclude({
+      prefix: '@@',
+      basepath: 'source/'
+    }))
+    .pipe($.inject(scriptsHead,
+      { ignorePath: [build, source],
+        addRootSlash: true,
+        starttag: '<!-- inject:head:{{ext}} -->'
+      }
+    ))
+    .pipe($.inject(es.merge(
+        vendorcss,
+        vendorjs
+      ),
+      {
+        name: 'bower',
+        ignorePath: [build, source],
+        addRootSlash: true
+      }
+      ))
+    .pipe($.inject(es.merge(
+      styles,
+      scripts
+    ),
+      {
+        ignorePath: [build, source],
+        addRootSlash: true
+      }
+    ))
+    .pipe(gulp.dest(build));
+});
+
+/* ====================================
+ * Clean up
+ * ==================================== */
+gulp.task('clean', del.bind(null, [build + '/*'], {dot: true}));
+
+/* ====================================
+ * Copy files
+ * ==================================== */
+gulp.task('copyfiles', function() {
+    return gulp.src(source + '/**/*.{ttf,woff,eof,svg,ico}')
+      .pipe($.plumber(plumberConfig))
+      .pipe(gulp.dest(build));
+});
+
+/* ====================================
+ * Gulp tasks
+ * ==================================== */
+
+// For local development
+gulp.task('default', ['clean'], function(){
+  runSequence(
+    ['html-default', 'images', 'copyfiles'],
+    ['serve']
+  );
+});
+
+// For staging/production deployment
+gulp.task('build', ['clean'], function(){
+  runSequence(
+    ['html-build', 'images', 'copyfiles']
+  );
+});
+
+/* ====================================
+ * Watch
+ * ==================================== */
+gulp.task('watch', function() {
+  gulp.watch(source + '/scss/**/*.scss', ['styles', reload]);
+
+  gulp.watch(source + '/js/**/*.js', ['jshint', 'scripts', reload]);
+
+  gulp.watch(source + '/img/**/*', ['images', reload]);
+
+  gulp.watch(source + '/htdocs/**/*', ['html-default', reload]);
+});
---
+++ source/js/script.js
@@ -0,0 +1,350 @@
+/* ====================================
+   Onload functions
+   ==================================== */
+
+
+var aeApp = aeApp || {};
+
+
+
+
+    aeApp.resize = function() {
+        aeApp.windowHeight      = $(window).height();
+        aeApp.windowWidth       = $(window).width();
+        aeApp.documentHeight    = $(document).height();
+        aeApp.transitionBegin   = (aeApp.documentHeight - aeApp.windowHeight) - 100;
+
+        $('.max-height').each(function() {
+          $(this).css('height', aeApp.windowHeight);
+        });
+
+        $('.image-container').find('img').each(function() {
+          var imageWidth = $(this).width();
+          var imageHeight = $(this).height();
+
+          $(this).css({
+            "margin-top": -imageHeight / 2,
+            "margin-left": -imageWidth / 2
+          })
+        });
+
+    };
+
+
+    aeApp.projectHover = function() {
+        $('a.project-link').hover(function(){
+            $(this).parents('article').addClass('activeHover');
+            $(this).parent().siblings('.cover-img').velocity("stop").velocity({
+                opacity: "1",
+            }, {
+                duration: 400,
+                easing: "easeInOut",
+                display: "block"
+            });
+        }, function(){
+            $(this).parents('article').removeClass('activeHover');
+            $(this).parent().siblings('.cover-img').velocity("stop").velocity({
+                opacity: "0",
+            }, {
+                duration: 400,
+                easing: "easeInOut",
+                display: "none"
+            });
+        });
+    };
+
+
+    aeApp.scroll = function() {
+        $(document).on('scroll', function(){
+            aeApp.scrollPosition    = $(window).scrollTop();
+            aeApp.footerOpacity     = (aeApp.scrollPosition - aeApp.transitionBegin) / 200;
+            if ( aeApp.scrollPosition > aeApp.transitionBegin ) {
+                $('.footer-bg').css('opacity', aeApp.footerOpacity);
+            } else {
+              $('.footer-bg').css('opacity', 0);
+            }
+        });
+    }
+
+    aeApp.slider = function(){
+      var slider = $('.slider');
+
+      slider.on('init afterChange',  function(event, slick, currentSlide, nextSlide){
+          var $status = $('.paging-info p');
+          var i = (currentSlide ? currentSlide : 0) + 1;
+          $status.text(i + '/' + slick.slideCount);
+      });
+
+      slider.slick({
+        dots: false,
+        infinite: false,
+        fade: true,
+        speed: 300,
+        slidesToShow: 1,
+        dots: false
+      });
+
+      slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+          $('.image-container').find('img').each(function() {
+            var imageWidth = $(this).width();
+            var imageHeight = $(this).height();
+
+            $(this).css({
+              "margin-top": -imageHeight / 2,
+              "margin-left": -imageWidth / 2
+            })
+          });
+      });
+
+    }
+
+    aeApp.lightboxIn = function(link, closeLink, lightbox){
+      link.click(function(e){
+        e.preventDefault();
+        console.log('clicked');
+        lightbox.velocity("stop").velocity({
+            opacity: "1",
+            top: 0
+        }, {
+            duration: 400,
+            easing: "easeInOut",
+            display: "block"
+        });
+      });
+      closeLink.click(function(e){
+        e.preventDefault();
+        lightbox.velocity("stop").velocity({
+            opacity: "0",
+            top: "24px"
+        }, {
+            duration: 400,
+            easing: "easeInOut",
+            display: "none"
+        });
+      });
+    }
+
+
+/* ====================================
+   Onload functions
+   ==================================== */
+
+;(function($, window, document) {
+
+  aeApp.lightboxIn($('.project-info-link a'), $('.close-info'), $('.project-info') );
+
+  $( window ).resize(function() {
+    aeApp.resize();
+    aeApp.scroll();
+  });
+
+  enquire.register("(max-width: 767px) ", {
+    match: function() {
+      $('a.project-link').unbind(aeApp.projectHover());
+      $('.cover-img').removeAttr("style")
+    },
+    unmatch: function() {
+      aeApp.projectHover();
+    }
+  });
+
+  enquire.register("(min-width: 768px) ", {
+    match: function() {
+      aeApp.projectHover();
+    },
+    unmatch: function() {
+      $('a.project-link').unbind(aeApp.projectHover());
+      $('.cover-img').removeAttr("style")
+    }
+  });
+
+  aeApp.slider();
+  aeApp.resize();
+
+
+  aeApp.windowHeight    = $(window).height();
+  aeApp.windowWidth     = $(window).width();
+  aeApp.documentHeight    = $(document).height();
+  aeApp.transitionBegin   = (aeApp.documentHeight - aeApp.windowHeight) - 100;
+
+  aeApp.scroll();
+
+
+
+
+
+
+} (window.jQuery, window, document));
+
+
+// showGrid();
+
+function showGrid() {
+  $('[class*="container"]').each(function() {
+    $(this).addClass("showgrid");
+  });
+
+  $('.row').each(function() {
+    $(this).addClass("showgrid");
+  });
+
+  $('[class*="col-"]').each(function() {
+    $(this).addClass("showgrid");
+  });
+}
+
+function hideGrid() {
+  $('.showgrid').each(function() {
+    $(this).removeClass("showgrid");
+  });
+}
+
+
+// console.log('%c Austen Ezzell ', 'background: #fff; color: #00b4e4; font-size: 26px');
+
+
+
+
+
+
+  // Include Polyfill to add pointer-events: non to browsers that don't support it.
+  // PointerEventsPolyfill.initialize({});
+
+  // STICKY HEADER
+
+  // $('.article-header').waypoint(function(direction) {
+  //   if(direction == 'down'){
+  //     $('.article-title').addClass('stuck');
+  //   } else {
+  //     $('.article-title').removeClass('stuck');
+  //   }
+  // }, {
+  //   offset: '-420px'
+  // });
+
+
+
+
+
+// aeApp.hoverArticleHeader = function() {
+
+//   var hoverElement = $('.article-hover');
+
+//   $('.nav-links a').hover(function(){
+//     var href = $(this).attr('href');
+//     $.ajax({
+//       url:href,
+//       type:'GET',
+//       success: function(data){
+//           hoverElement.attr('style', $(data).find('.article-header').attr('style') );
+//       }
+//     });
+//     hoverElement.addClass('hover');
+//   }, function(){
+//       hoverElement.removeClass('hover');
+//   });
+
+//   $('.nav-links a').click(function(){
+//     hoverElement.addClass('clicked')
+//   });
+
+// };
+
+
+
+
+
+// aeApp.nextIssue = function() {
+
+//   var nextIssueLink = $('.next-issue-link');
+//   var href = nextIssueLink.attr('href');
+
+//   $.ajax({
+//     url:href,
+//     type:'GET',
+//     success: function(data){
+//         $('.next-issue-img').attr('style', $(data).find('.article-header').attr('style') );
+//     }
+//   });
+
+//   $('.next-issue-img').velocity({opacity: "0"});
+
+//   nextIssueLink.hover(function(){
+
+//     $('.next-issue-img').velocity("stop").velocity({
+//         opacity: "1",
+//     }, {
+//         duration: 400,
+//         easing: "easeInOut",
+//         display: "block"
+//     });
+//   }, function(){
+//     $('.next-issue-img').velocity("stop").velocity({
+//         opacity: "0",
+//     }, {
+//         duration: 400,
+//         easing: "easeInOut",
+//         display: "none"
+//     });
+//   });
+
+// }
+
+
+
+
+// aeApp.readBar = function() {
+
+//     aeApp.windowHeight    = $(window).height();
+//     aeApp.windowWidth     = $(window).width();
+
+//   // Reading Progress Indicator
+//     var progressBarWidth  = aeApp.windowWidth-40;
+
+//     var getMax = function(){
+//       return $(document).height() - $(window).height() - $('.article-header').height();
+//     }
+
+//     var getValue = function(){
+//       return $(window).scrollTop() - $('.article-header').height();
+//     }
+
+//     if ('max' in document.createElement('progress')) {
+//       var progressBar = $('progress');
+
+//       progressBar.attr({ max: getMax() }).css("width", progressBarWidth);
+
+//       $(document).on('scroll', function(){
+//         progressBar.attr({ value: getValue() });
+//       });
+
+//       $(window).resize(function(){
+//         progressBar.attr({ max: getMax(), value: getValue() });
+//       });
+
+//     } else {
+
+//       var progressBar = $('.progress-bar'),
+//           max         = getMax(),
+//           value,
+//           width;
+
+//       var getWidth = function() {
+//         value = getValue();
+//         width = (value/max) * 100;
+//         width = width + '%';
+//         return width;
+//       }
+
+//       var setWidth = function(){
+//         progressBar.css({ width: getWidth() });
+//       }
+
+//       $(document).on('scroll', setWidth);
+//       $(window).on('resize', function(){
+//         max = getMax();
+//         setWidth();
+//       });
+//     }
+
+// }
